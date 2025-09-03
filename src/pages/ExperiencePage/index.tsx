b970d09940d1732581d67556dbb617e9c34eef54
RoyCDev <rcheung0117@gmail.com>
2025-09-02 20:52:24 -0700

    responsive experience page

--- src/pages/ExperiencePage/index.tsx
+++ src/pages/ExperiencePage/index.tsx
@@ -7,23 +7,23 @@ function ExperiencePage() {
   return (
     <ul className="mt-15">
       {jobs.map((job, i) => (
-        <li key={i} className="flex items-start gap-x-6">
-          <div className="min-w-46 text-end">
-            <p className="text-lg text-accent">{job.start} - {job.end}</p>
-            <p className="text-2xl leading-6 mt-1">{job.title}</p>
-            <div className="flex items-center gap-x-1 w-fit justify-self-end bg-primary px-2 mt-5">
+        <li key={i} className="grid grid-cols-[auto_1fr] grid-rows-[auto_auto] gap-3 md:flex md:items-start md:gap-6">
+          <div className="divider divider-accent divider-start divider-horizontal before:w-0.25! after:w-0.25! gap-0! row-span-2 md:order-2">
+            <Avatar src={job.logo} alt={job.company} className="w-10 lg:w-12.5" />
+          </div>
+          <div className="md:min-w-38 lg:min-w-46 md:text-end md:order-1">
+            <p className="text-accent text-sm lg:text-lg">{job.start} - {job.end}</p>
+            <p className="leading-6 mt-1 text-xl lg:text-2xl">{job.title}</p>
+            <div className="flex items-center gap-x-1 w-fit bg-primary px-2 mt-6 -mb-2 text-sm lg:text-base md:ml-auto">
               <FaLocationDot />
               <p className="mt-0.5">{job.location}</p>
             </div>
           </div>
-          <div className="divider divider-accent divider-start divider-horizontal before:w-0.25! after:w-0.25! gap-0!">
-            <Avatar src={job.logo} alt={job.company} className="w-12.5" />
-          </div>
-          <div className={i !== jobs.length - 1 ? "pb-20" : ""}>
-            <p className="text-4xl">{job.company}</p>
-            {job.description && <p className="font-kumbh text-xl mt-5">{job.description}</p>}
-            <p className="text-2xl mt-5 mb-3">Accomplishments:</p>
-            <ul className="font-kumbh grid grid-cols-3 gap-x-3">
+          <div className={i !== jobs.length - 1 ? "pb-20 md:order-3" : "md:order-3"}>
+            <p className="text-2xl md:text-3xl lg:text-4xl">{job.company}</p>
+            {job.description && <p className="font-kumbh mt-5 sm:text-lg lg:text-xl">{job.description}</p>}
+            <p className="mt-5 mb-3 text-lg sm:text-xl lg:text-2xl">Accomplishments:</p>
+            <ul className="font-kumbh grid gap-3 lg:grid-cols-3 ">
               {job.responsibilities.map((responsibility, i) => (
                 <InfoItem key={i + 1} info={responsibility} number={i + 1} />
               ))}
